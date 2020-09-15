d0c8ddc2722cd0b23bfa84497d47de41890a1ce4
gcpetri <gcpetri@outlook.com>
2020-09-15 16:31:21 -0500

    we have a FP character now

---
+++ VoltageSource/Assets/Scripts/FP_gunScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class FP_gunScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    
+    public Camera FP_cam;
+    public ParticleSystem laserShot;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        laserShot.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(FP_cam.transform.position, FP_cam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
