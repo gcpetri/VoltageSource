d2fe2f497848b56d9f20acaefa0d4a1d962cce11
Erik Priest <eriktpriest@gmail.com>
2020-09-24 20:49:12 -0500

    Created another sample level

---
+++ VoltageSource/Assets/Scripts/PickupChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupChecker : MonoBehaviour
+{
+    private Camera _camera;
+    public LayerMask _pickupMask;
+    [SerializeField] private float pickUpDistance = 5f;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        // Do a short wave raycast to check for there
+        RaycastHit hit;
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit, pickUpDistance, _pickupMask)){
+            if (hit.distance <= pickUpDistance)
+            {
+                hit.transform.GetComponent<Pickupable>().PickupObject();
+            }
+        }
+    }
+}
---
+++ VoltageSource/Assets/Scripts/Pickupable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(SphereCollider))]
+public class Pickupable : MonoBehaviour
+{
+   
+    public void PickupObject()
+    {
+        Debug.Log("Picked up");
+    }
+}
