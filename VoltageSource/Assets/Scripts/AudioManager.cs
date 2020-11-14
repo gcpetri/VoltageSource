02ec0339f27c0004a439cf2863efca3f9c21e5a6
Gregory Petri <gcpetri@outlook.com>
2020-11-13 19:08:33 -0600

    music

--- VoltageSource/Assets/Scripts/AudioManager.cs
+++ VoltageSource/Assets/Scripts/AudioManager.cs
@@ -7,22 +7,24 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
     private const string VolumeKey = "VolumeValue";
+    private AudioSource _audio;
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(this);
+            return;
         }
-        else
-        {
+        if (Instance == this)
             Destroy(this);
-        }
     }
 
     private void Start()
     {
-        DontDestroyOnLoad(this);
+        _audio = GetComponent<AudioSource>();
+        _audio.Play();
     }
 
     public void SetVolume(float val)
