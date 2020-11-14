4b42ddd594719e6bfae6b274296963ff3ea118a6
Erik Priest <eriktpriest@gmail.com>
2020-11-13 19:50:08 -0600

    audio update and hypothetical update for health

--- VoltageSource/Assets/Scripts/AudioManager.cs
+++ VoltageSource/Assets/Scripts/AudioManager.cs
@@ -24,12 +24,14 @@ public class AudioManager : MonoBehaviour
     private void Start()
     {
         _audio = GetComponent<AudioSource>();
+        _audio.volume = PlayerPrefs.GetFloat(VolumeKey);
         _audio.Play();
     }
 
     public void SetVolume(float val)
     {
         PlayerPrefs.SetFloat(VolumeKey, val);
+        _audio.volume = PlayerPrefs.GetFloat(VolumeKey);
     }
     
 }
